ac11c1359d3886bdf8316ea6c180ae4dde771c44
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-05-05 14:46:05 +0200

    complete index page href specifications

--- Deliverable 2/Prototypes/A/projectIndexPHPForm.php
+++ Deliverable 2/Prototypes/A/projectIndexPHPForm.php
@@ -155,22 +155,34 @@
                 <div class="carousel_container">
                     <ul class="carousel_track">
                         <li class="carousel_slide current-slide">
-                            <img src="carousel_1.webp" alt="" class="carousel_img">
+                            <a href="">
+                                <img src="carousel_1.webp" alt="" class="carousel_img">
+                            </a>
                         </li>
                         <li class="carousel_slide">
-                            <img src="carousel_2.webp" alt="" class="carousel_img">
+                            <a href="projectProductPHPForm.php?prod_id=30">
+                                <img src="carousel_2.webp" alt="" class="carousel_img">
+                            </a>
                         </li>
                         <li class="carousel_slide">
-                            <img src="carousel_3.webp" alt="" class="carousel_img">
+                            <a href="projectProductPHPForm.php?prod_id=13">
+                                <img src="carousel_3.webp" alt="" class="carousel_img">
+                            </a>
                         </li>
                         <li class="carousel_slide">
-                            <img src="carousel_4.webp" alt="" class="carousel_img">
+                            <a href="projectProductPHPForm.php?prod_id=41">
+                                <img src="carousel_4.webp" alt="" class="carousel_img">
+                            </a>
                         </li>
                         <li class="carousel_slide">
-                            <img src="carousel_5.webp" alt="" class="carousel_img">
+                            <a href="projectProductPHPForm.php?prod_id=36">
+                                <img src="carousel_5.webp" alt="" class="carousel_img">
+                            </a>
                         </li>
                         <li class="carousel_slide">
+                        <a href="projectProductPHPForm.php?prod_id=9">
                             <img src="carousel_6.webp" alt="" class="carousel_img">
+                        </a>
                         </li>
                     </ul>
                 </div>
@@ -188,9 +200,15 @@
         </div>
 
         <div class='left-placart'>
+            <a href="projectCataloguePHPForm.php?cat=africa_ceremony">
+                <img src="shop_traditional.webp" class="promo_img_left">
+                <p class= "promo_text_left">Shop Traditional <br> Shop Proudly SA<br><br><br></p>
+            </a>
         </div>
 
         <div class='right-placart'>
+            <p class="promo_text_right">Free Delivery <br> When You Spend <br> R1000 Or More<br><br></p>
+            <img src="cargo-truck.png" class="promo_img_right">
         </div>
 
       </div>
