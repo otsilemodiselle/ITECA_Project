1c4dc09527ce89349a782368215e622bf575f7ba
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-05-11 15:57:25 +0200

    configure wishlist management buttons for logged in users

--- Deliverable 2/Prototypes/A/projectWishlistPHPForm.php
+++ Deliverable 2/Prototypes/A/projectWishlistPHPForm.php
@@ -3,6 +3,42 @@
 
   session_start();
 
+  if (isset($_POST['wishlist-action']))
+  {
+    $action = $_POST['wishlist-action'];
+
+      switch ($action)
+    {
+      case 'Delete':
+        $prod_id = ($_POST['prod_id']);
+        if(isset($_SESSION['customer_id']))
+        {
+          $customer_id = $_SESSION['customer_id'];
+          $query = "SELECT coll_id
+                  FROM collection
+                  WHERE customer_id = $customer_id AND prod_id = $prod_id;";
+          
+          $result = queryMysql($query);
+
+          $row = $result->fetch();
+          $coll_id = $row['coll_id'];
+
+          $queryDeleteInCollection = "DELETE FROM collection WHERE coll_id = $coll_id;";
+          queryMysql($queryDeleteInCollection);          
+        }
+        
+        unset($_POST['wishlist-action']);
+        break;
+      case 'Add to Cart':
+        $prod_id = ($_POST['prod_id']);
+        header("Location: projectProductPHPForm.php?prod_id=$prod_id");
+        exit;
+        unset($_POST['wishlist-action']);
+        break;
+      
+    }
+  }
+
   require_once 'above_nav_content.php';
 
   echo<<<_END
@@ -23,33 +59,46 @@
               WHERE c.customer_id = $customer_id;";
     $result = queryMysql($query);
     
-    while ($row = $result->fetch())
+    if ($result->rowCount())
     {
-      $prod_id = $row['prod_id'];
+      while ($row = $result->fetch())
+      {
+        $prod_id = $row['prod_id'];
 
-      $queryProdData =  "SELECT prod_img, prod_name, price
-                        FROM product
-                        WHERE prod_id = $prod_id;";
-      $prodDataResult = queryMysql($queryProdData);
+        $queryProdData =  "SELECT prod_img, prod_name, price
+                          FROM product
+                          WHERE prod_id = $prod_id;";
+        $prodDataResult = queryMysql($queryProdData);
 
-      $row = $prodDataResult->fetch();
-      $prod_img = $row['prod_img'];
-      $prod_name = $row['prod_name'];
-      $price = $row['price'];
+        $row = $prodDataResult->fetch();
+        $prod_img = $row['prod_img'];
+        $prod_name = $row['prod_name'];
+        $price = $row['price'];
+
+        echo<<<_END
+        <form method="post" action="projectWishlistPHPForm.php">
+            <div class='wishlist-banner clearfix'>
+              <img src="Images/img/$prod_img" alt="" class="small-thumbnail">
+              <p class="prod-price-panel">R$price</p> 
+              <p class="prod-name-panel">$prod_name</p>
+              <input type="hidden" name="prod_id" value="$prod_id">
+              <input type="submit" name="wishlist-action" class="wish-delete" value="Delete">
+              <input type="submit" name="wishlist-action" class="wish-cart" value="Add to Cart">
+            </div>
+        </form>
+        _END;
+      }
 
       echo<<<_END
-      <form method="post" action="projectWishlistPHPForm.php">
-        <input type="hidden" name="prod_id" value="$prod_id>
-        <a href="projectProductPHPForm.php?prod_id=$prod_id">
-          <div class='wishlist-banner clearfix'>
-            <img src="Images/img/$prod_img" alt="" class="small-thumbnail">
-            <input class="panel-selection" type="checkbox">
-            <p class="prod-price-panel">$price</p> 
-            <p class="prod-name-panel">$prod_name</p>
-            <input type="submit" name="wish-delete" class="wish-delete" value="Delete">
-          </div>
-        </a>
-      </form>
+      </div>
+      </section>
+      _END;
+    }
+    else
+    {
+      echo<<<_END
+      </div>
+      </section>
       _END;
     }
   }
@@ -57,20 +106,6 @@
   {
 
   }
-
-  echo<<<_END
-  </div>
-  <button class="wish-clear">
-      Clear Wishlist
-  </button>
-  <button class="wish-delete-selection">
-      Delete Selected
-  </button>
-  <button class="wish-go-to-cart">
-      Go to Cart
-  </button>
-  </section>
-  _END;
 
   echo<<<_END
   <footer>
