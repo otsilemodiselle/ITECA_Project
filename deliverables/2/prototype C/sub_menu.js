9d3801be077cc137e483e650ff352b7735d1e7c2
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-06-16 03:03:25 +0200

    update js script handling mobile view burger menu

--- deliverables/2/prototype C/sub_menu.js
+++ deliverables/2/prototype C/sub_menu.js
@@ -1,37 +1,59 @@
-document.addEventListener
-(
-  "DOMContentLoaded",
-  function()
-  {
-    const menuButton = document.getElementsByClassName("btn-burger");
-    const submenu = document.getElementsByClassName("sub-menu");
+document.addEventListener("DOMContentLoaded", function() {
+  const menuButton = document.querySelector(".btn-burger");
+  const submenu = document.querySelector(".sub-menu");
 
-    menuButton.addEventListener
-    (
-      "click",
-      function(event)
-      {
-        event.stopPropagation();
-        submenu.classList.toggle("visible");
-      }
-    );
+  if (menuButton && submenu) {
+    menuButton.addEventListener("click", function(event) {
+      event.stopPropagation();
+      submenu.classList.toggle("visible");
+    });
 
-    document.addEventListener
-    (
-      "click",
-      function()
-      {
-        submenu.classList.remove("visible");
-      }
-    );
+    document.addEventListener("click", function() {
+      submenu.classList.remove("visible");
+    });
 
-    submenu.addEventListener
-    (
-      "click",
-      function(event)
-      {
-        event.stopPropagation();
-      }
-    );
+    submenu.addEventListener("click", function(event) {
+      event.stopPropagation();
+    });
+  } else {
+    console.error('Menu button or submenu not found');
   }
-);
+});
+
+// document.addEventListener
+// (
+//   "DOMContentLoaded",d
+//   function()
+//   {
+//     const menuButton = document.getElementsByClassName("btn-burger");
+//     const submenu = document.getElementsByClassName("sub-menu");
+
+//     menuButton.addEventListener
+//     (
+//       "click",
+//       function(event)
+//       {
+//         event.stopPropagation();
+//         submenu.classList.toggle("visible");
+//       }
+//     );
+
+//     document.addEventListener
+//     (
+//       "click",
+//       function()
+//       {
+//         submenu.classList.remove("visible");
+//       }
+//     );
+
+//     submenu.addEventListener
+//     (
+//       "click",
+//       function(event)
+//       {
+//         event.stopPropagation();
+//       }
+//     );
+//   }
+// );
