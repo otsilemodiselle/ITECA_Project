e339b3575a327b12ce2955efb166dac5e9465c10
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-06-19 20:21:52 +0200

    update login modal window js script

--- deliverables/2/prototype C/login_modal.js
+++ deliverables/2/prototype C/login_modal.js
@@ -1,25 +1,25 @@
 var loginModal = document.getElementById("login-modal");
 
-var btn = document.getElementById("open-login-modal");
+var loginBtn = document.getElementById("open-login-modal");
 
-var btn2 = document.getElementById("open-login-modal-sub");
+var loginBtn2 = document.getElementById("open-login-modal-sub");
 
-var span = document.getElementsByClassName("close-login")[0];
+var closeLoginSpan = document.getElementsByClassName("close-login")[0];
 
-btn.onclick = function() {
+loginBtn.onclick = function() {
   loginModal.style.display = "block";
 }
 
-btn2.onclick = function() {
+loginBtn2.onclick = function() {
   loginModal.style.display = "block";
 }
 
-span.onclick = function() {
+closeLoginSpan.onclick = function() {
   loginModal.style.display = "none";
 }
 
 window.onclick = function(event)  {
   if (event.target == modal) {
-    modal.style.display = "none";
+    loginModal.style.display = "none";
   }
 }
