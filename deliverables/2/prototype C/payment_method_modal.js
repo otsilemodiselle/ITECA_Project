5bc0fa25488b6a9a315faa240e84f399014f5531
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-06-19 20:20:49 +0200

    complete payment method modal window

--- deliverables/2/prototype C/payment_method_modal.js
+++ deliverables/2/prototype C/payment_method_modal.js
@@ -1,23 +1,19 @@
-var loginModal = document.getElementById("payment-method-modal");
+var paymentMethodModal = document.getElementById("payment-method-modal");
 
-var btn = document.getElementById("open-payment-method-modal");
+var paymentMethodBtn = document.getElementById("open-payment-method-modal");
 
-var span = document.getElementsByClassName("close-payment-method")[0];
+var closePaymentMethod = document.getElementsByClassName("close-payment-method")[0];
 
-btn.onclick = function() {
-  loginModal.style.display = "block";
+paymentMethodBtn.onclick = function() {
+  paymentMethodModal.style.display = "block";
 }
 
-btn2.onclick = function() {
-  loginModal.style.display = "block";
-}
-
-span.onclick = function() {
-  loginModal.style.display = "none";
+closePaymentMethod.onclick = function() {
+  paymentMethodModal.style.display = "none";
 }
 
 window.onclick = function(event)  {
   if (event.target == modal) {
-    modal.style.display = "none";
+    paymentMethodModal.style.display = "none";
   }
 }
