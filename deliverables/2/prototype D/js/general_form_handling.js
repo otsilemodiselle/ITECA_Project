3c8e24df402882775643e78bd26e87d11333c435
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-06-23 17:47:11 +0200

    amend support files

--- deliverables/2/prototype D/js/general_form_handling.js
+++ deliverables/2/prototype D/js/general_form_handling.js
@@ -5,3 +5,15 @@ function clearLoginEmail() {
 function clearLoginPassword() {
     document.getElementById('login-password-box').value = '';
 }
+
+function sizeCheck() {
+  var selectElement = document.getElementsByName("sizes")[0];
+  var cartButton = document.getElementsByClassName("cart-button")[0];
+
+  if (selectElement.selectedIndex === 0 && selectElement.value === "") {
+      cartButton.disabled = true;
+  } else {
+      cartButton.disabled = false;
+  }
+}
+
